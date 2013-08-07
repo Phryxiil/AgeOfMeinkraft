177ece0326f146bcb2d2c0f47c951ee26565204e
Zarubinator <bio.hazrd-@hotmail.com>
2013-08-07 00:05:24 -0400

    derp derp

---
+++ src/main/java/me/iaccidentally/ageofmeinkraft/Main.java
@@ -0,0 +1,17 @@
+package me.iaccidentally.ageofmeinkraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin
+{
+    @Override
+	public void onEnable()
+	{
+	}
+    @Override
+    public void onDisable()
+	{
+		
+	}
+}
